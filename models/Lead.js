481f3fb2e61ad3654cf8df76b21d6a1542126b50
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-02-16 12:24:25 +0100

    Create model for Lead Table

---
+++ models/Lead.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+    let Lead = sequelize.define('Lead', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+
+            allowNull: false,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return Lead;
+};
