746423782ab256326addb9e93aebdbf0f9661fb2
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-02-16 15:24:55 +0100

    Migrate all pages to bootstrap
    Also created separate leads.pug file

--- controllers/landing.js
+++ controllers/landing.js
@@ -15,7 +15,7 @@ exports.submit_lead = function(req, res, next) {
 
 exports.show_leads = function(req, res, next) {
     models.Lead.findAll().then(leads => {
-        res.render('landing', { title: 'Express', leads: leads });
+        res.render('lead/leads', { title: 'Express', leads: leads });
     }).catch(error => console.log(error) );
 };
 
@@ -25,7 +25,7 @@ exports.show_lead = function(req, res, next) {
             id: req.params.lead_id
         }
     }).then(lead => {
-        res.render('lead', { lead: lead })
+        res.render('lead/lead', { lead: lead })
     });
 };
 
