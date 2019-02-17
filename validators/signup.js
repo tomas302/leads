97c85162a5ce641cd2fd3e15e13859d117b72e4b
Tomás Dornell <tomasdornell.gtd@gmail.com>
2019-02-17 12:51:32 +0100

    Implemented error handling and form validation for signup route

---
+++ validators/signup.js
@@ -0,0 +1,31 @@
+let models = require('../models');
+
+let validator = require('validator');
+
+const validateCreateUserFields = (errors, req) => {
+    if (!validator.isEmail(req.body.email)) {
+        errors["email"] = "Please use a valid email.";
+    }
+    if (!validator.isAscii(req.body.password)) {
+        errors["password"] = "Invalid characters in password, please try another one.";
+    }
+    if (!validator.isLength(req.body.password, { min: 8, max: 25 })) {
+        errors["password"] = "Please ensure that your password has a minimun of 8 characters."
+    }
+};
+
+exports.validateUser = (errors, req) => {
+    return new Promise((resolve, reject) => {
+        validateCreateUserFields(errors, req);
+        return models.User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(u => {
+            if (u !== null) {
+                errors["email"] = "Email is already in use. Please login or reset your password.";
+            }
+            resolve(errors)
+        });
+    });
+};
